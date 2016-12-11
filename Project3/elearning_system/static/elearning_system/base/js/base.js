d2cdefc8e38d0899ae91648dd2588a490da597af
haminhcong <haminhcongbkhn@gmail.com>
2016-12-11 15:11:28 +0700

    move to new base.html

--- Project3/elearning_system/static/elearning_system/base/js/base.js
+++ Project3/elearning_system/static/elearning_system/base/js/base.js
@@ -4,7 +4,6 @@
 
 var pagination = $('.pagination_btn');
 pagination.click(function () {
-    console.log('dlkjfasldjfasdk');
    var self = $(this);
     $('.pagination_btn').removeClass('active');
     self.addClass('active');

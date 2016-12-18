c8951a55009715aa84acd4e9b4064c6868df5396
huynhducbk95 <huynhducpro95@gmail.com>
2016-12-18 16:39:48 +0700

    update base.html

--- Project3/elearning_system/view/user/views.py
+++ Project3/elearning_system/view/user/views.py
@@ -1,130 +1,108 @@
 # coding=utf-8
 from django.shortcuts import render, redirect
-import django
 from django.http import HttpResponse
 import json
-import datetime
-from elearning_system.models import User, ExerciseWebServer, Tag, UserSolveExercise, Role,UserRole,ErrorMessage
-
+from elearning_system.models import User, ExerciseWebServer, Tag, Role
 
 def index(request):
-    topic = ['Lập trình hướng đối tượng', 'Tin học đại cương', 'Lập trình java', 'Kỹ thuật lập trình',
-             'Lập trình song song', ]
+    role_moderator = Role.objects.get(pk=2)
+    user_test = User.objects.get(pk=1)
+    role_moderator.user_list.add(user_test)
 
-    top_user = [{
-        'username': 'Username 1',
-        'passed': 15,
-        'message': 18,
-        'contributed': 35,
-    }, {
-        'username': 'Username 2',
-        'passed': 15,
-        'message': 18,
-        'contributed': 35,
-    }, {
-        'username': 'Username 3',
-        'passed': 15,
-        'message': 18,
-        'contributed': 35,
-    }, {
-        'username': 'Username 4',
-        'passed': 15,
-        'message': 18,
-        'contributed': 35,
-    }, {
-        'username': 'Username 5',
-        'passed': 15,
-        'message': 18,
-        'contributed': 35,
-    }, {
-        'username': 'Username 6',
-        'passed': 15,
-        'message': 18,
-        'contributed': 35,
-    }, {
-        'username': 'Username 7',
-        'passed': 15,
-        'message': 18,
-        'contributed': 35,
-    }, {
-        'username': 'Username 8',
-        'passed': 15,
-        'message': 18,
-        'contributed': 35,
-    }]
-    top_exercise = [{
-        'name': 'Exercise 1',
-        'description': 'Description for exercise is a short sentence .... ',
-        'viewed': 10,
-        'passed': 25,
-        'messaged': 14,
-    }, {
-        'name': 'Exercise 2',
-        'description': 'Description for exercise is a short sentence .... ',
-        'viewed': 10,
-        'passed': 25,
-        'messaged': 14,
-    }, {
-        'name': 'Exercise 3',
-        'description': 'Description for exercise is a short sentence .... ',
-        'viewed': 10,
-        'passed': 25,
-        'messaged': 14,
-    }, {
-        'name': 'Exercise 4',
-        'description': 'Description for exercise is a short sentence .... ',
-        'viewed': 10,
-        'passed': 25,
-        'messaged': 14,
-    }, ]
+    user_list = User.objects.all().order_by('-contribute_number')
+    count = 1
+    top_user_list = []
+    for user in user_list:
+        if count == 9:
+            break
+        else:
+            user_top = {
+                'username': user.user_name,
+                'passed': user.solve_number,
+                'contributed': user.contribute_number,
+            }
+            top_user_list.append(user_top)
+            count += 1
+    exercise_list = ExerciseWebServer.objects.all().order_by('-solve_number')
+    count = 1
+    top_exercise_list = []
+    for exercise in exercise_list:
+        if count == 5:
+            break
+        else:
+            exercise_top = {
+                'name': exercise.exercise_name,
+                'description': exercise.exercise_description,
+                'passed': exercise.solve_number,
+                'contributor': exercise.contributer_id.user_name,
+                'view':exercise.view_number,
+            }
+            top_exercise_list.append(exercise_top)
+            count += 1
+    tag_list_db = Tag.objects.all()
+    tag_list = []
+    for tag in tag_list_db:
+        tag_info = {
+            'name': tag.tag_name,
+            'id': tag.id
+        }
+        tag_list.append(tag_info)
     result = {
         'title': 'Coding School',
-        'topic_list': topic,
-        'top_exercise': top_exercise,
-        'top_user': top_user,
+        'topic_list': tag_list,
+        'top_exercise': top_exercise_list,
+        'top_user': top_user_list,
+        'length': len(top_exercise_list),
     }
-    # create database of tag
-    user = User.objects.get(pk=3)
-
-    for tag in topic:
-        tag_obj = Tag(tag_name=tag)
-        tag_obj.save()
-    tag = Tag.objects.get(pk=2)
-    
-    for ex in top_exercise:
-        ex1 = ExerciseWebServer(exercise_name=ex['name'],
-                                exercise_descrip=ex['description'],
-                                view_number=ex['viewed'],
-                                contributer_id=user.id,
-                                solve_number=ex['passed'],
-                                created_date=datetime.datetime.now(),
-                                tag_id=tag,
-                                approver_id=user.id,
-                                )
-        ex1.save()
-    for user in top_user:
-        us = User(user_name=user['username'],
-                  account_name=user['username'],
-                  password=user['username'],
-                  contribute_number=user['contributed'],
-                  email_address=user['username'],
-                  block_status='Active',
-                  solve_number=user['passed'])
-        us.save()
-
-    user_list = User.objects.all().order_by('contribute_number')
-
-    if 'username' in request.session:
-        result['username'] = request.session['username']
+    context = {
+        'account_name': 'account_name',
+        'is_login': 'true',
+        'user_role_list':[
+            'admin',
+            'moderator',
+            'user'
+        ]
+    }
+    role_list = []
+    if 'is_login' in context:
+        for role in context['user_role_list']:
+            role_list.append(role)
+        result['username'] = context['account_name']
+        result['role_list'] = role_list
     return render(request, 'elearning_system/user/index.html', {'result': result})
 
 
 def list_ex_of_topic(request):
-    # print('x')
     if request.method == 'GET':
         tag = request.GET.get('tag', None)
         exercises = ExerciseWebServer.objects.all()
-    return render(request, 'elearning_system/user/list_ex_of_topic.html', {'title': 'registry'})
+        exercise_list = []
+        for exercise in exercises:
+            if exercise.tag_id.id == int(tag):
+                exercise_info = {
+                    'name': exercise.exercise_name,
+                    'description': exercise.exercise_description,
+                    'passed': exercise.solve_number,
+                    'contributor': exercise.contributer_id.user_name,
+                    'view': exercise.view_number,
+                }
+                exercise_list.append(exercise_info)
+        tag_list_db = Tag.objects.all()
+        tag_list = []
+        tag_db = Tag.objects.get(pk=tag)
+        for tag in tag_list_db:
+            tag_info = {
+                'name': tag.tag_name,
+                'id': tag.id
+            }
+            tag_list.append(tag_info)
+        result = {
+            'exercise_number': len(exercise_list),
+            'exercise_list': exercise_list,
+            'topic_list': tag_list,
+            'tag_name': tag_db.tag_name
+        }
+        return render(request, 'elearning_system/user/list_ex_of_topic.html', {'result': result})
 
 
 def contact(request):
@@ -133,28 +111,85 @@ def contact(request):
 
 
 def search(request):
-    result = {
-
-    }
     if (request.method == "GET"):
-        search = request.GET.get('search', None)
-        result['search'] = search
+        search_keyword = request.GET.get('search', None)
+        search_keyword = search_keyword.lower()
+        tag_list_db = Tag.objects.all()
+        tag_list = []
+        for tag in tag_list_db:
+            tag_info = {
+                'name': tag.tag_name,
+                'id': tag.id
+            }
+            tag_list.append(tag_info)
 
-    return render(request, 'elearning_system/user/search.html', result)
+            #     search by keyword
+        exercise_list = ExerciseWebServer.objects.all()
+        exercise_list_result = []
+        exercise_list_db_result = []
+        for exercise in exercise_list:
+            exercise_name = exercise.exercise_name.lower()
+            if exercise_name.__contains__(search_keyword):
+                exercise_info = {
+                    'name': exercise.exercise_name,
+                    'description': exercise.exercise_description,
+                    'passed': exercise.solve_number,
+                    'contributor': exercise.contributer_id.user_name,
+                    'view': exercise.view_number,
+                }
+                exercise_list_result.append(exercise_info)
+                exercise_list_db_result.append(exercise)
+        for exercise in exercise_list:
+            if not exercise in exercise_list_db_result:
+                description = exercise.exercise_description.lower()
+                if description.__contains__(search_keyword):
+                    exercise_info = {
+                        'name': exercise.exercise_name,
+                        'description': exercise.exercise_description,
+                        'passed': exercise.solve_number,
+                        'contributor': exercise.contributer_id.user_name,
+                        'view': exercise.view_number,
+                    }
+                    exercise_list_result.append(exercise_info)
+                    exercise_list_db_result.append(exercise)
+        result = {
+            'exercise_number': len(exercise_list_result),
+            'title': 'Search',
+            'keyword': search_keyword,
+            'exercise_list': exercise_list_result,
+            'topic_list': tag_list,
+        }
+        return render(request, 'elearning_system/user/search.html', {'result': result})
 
 
 def compare_exercise(request):
     result = {}
     if request.method == 'GET':
         option = request.GET.get('option', None)
-        if option == 'View':
+        page = request.GET.get('page', None)
+        if option == 'view':
             value = 'view_number'
-        elif option == 'Passed':
+        elif option == 'passed':
             value = 'solve_number'
         else:
             value = 'created_date'
-        exercise_list = ExerciseWebServer.objects.all().order_by(value)
-        print exercise_list
+        if page == 'index':
+            exercise_list = ExerciseWebServer.objects.all().order_by('-'+value)
+        else:
+            exercise_list = []
+        exercise_list_result = []
+        for exercise in exercise_list:
+            exercise_info = {
+                'name': exercise.exercise_name,
+                'description': exercise.exercise_description,
+                'passed': exercise.solve_number,
+                'contributor': exercise.contributer_id.user_name,
+                'view': exercise.view_number,
+            }
+            exercise_list_result.append(exercise_info)
+        result = {
+            'exercise_list':exercise_list_result,
+        }
     return HttpResponse(json.dumps(result), content_type='application/json')
 
 
@@ -164,6 +199,7 @@ def login(request):
         password = request.POST.get('password', None)
         user_list = User.objects.all()
         error = False
+
         for user in user_list:
             if user.user_name == username and user.password == password:
                 error = True
@@ -172,8 +208,10 @@ def login(request):
             if 'username' in request.session:
                 del request.session['username']
             request.session['username'] = username
+            user_obj = User.objects.get(user_name=username)
             result = {
                 'result': 'successful',
+                'account_name': user_obj.account_name,
             }
         else:
             result = {
@@ -184,10 +222,13 @@ def login(request):
 
 def logout(request):
     if request.method == 'GET':
-        if 'username' in request.session:
-            del request.session['username']
+        user = None
+        if 'user_name' in request.session:
+            user = request.session['user_name']
+            del request.session['user_name']
+        user_obj = User.objects.get(account_name=user)
         result = {
-            'result': 'successful'
+            'result': 'successful',
         }
         return redirect(to='index')
 
@@ -212,10 +253,11 @@ def registry(request):
                     account_name=account_name,
                     block_status=block_status)
         user.save()
-        if 'username' in request.session:
-            del request.session['username']
-        request.session['username'] = username
+        if 'user_name' in request.session:
+            del request.session['user_name']
+        request.session['user_name'] = username
         result = {
-            'result': 'successful'
+            'result': 'successful',
+            'account_name': account_name,
         }
         return HttpResponse(json.dumps(result), content_type='application/json')

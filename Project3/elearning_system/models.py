2ded3aeea9cff6057802792177a204f81275f381
huynhducbk95 <huynhducpro95@gmail.com>
2016-12-21 01:16:45 +0700

    change on_delete, name attribute of models.py

--- Project3/elearning_system/models.py
+++ Project3/elearning_system/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class User(models.Model):
-    full_name = models.CharField(max_length=20, )
+    full_name = models.CharField(max_length=50, )
     password = models.CharField(max_length=30, )
     user_name = models.CharField(max_length=30, )
-    email_address = models.CharField(max_length=30, )
+    email_address = models.CharField(max_length=50, )
     block_status = models.CharField(max_length=10, )
     contribute_number = models.IntegerField(default=0)
     solve_number = models.IntegerField(default=0)
@@ -25,7 +25,7 @@ class Role(models.Model):
 
 
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=30, )
+    tag_name = models.CharField(max_length=50, )
 
     def __str__(self):
         return self.tag_name
@@ -35,11 +35,10 @@ class ExerciseWebServer(models.Model):
 
     view_number = models.IntegerField(default=0)
     solve_number = models.IntegerField(default=0)
-    contributer_id = models.ForeignKey(User, null=False, related_name='user_contributer')
-    approver_id = models.ForeignKey(User, null=True, related_name='user_approver')
-    created_date = models.DateTimeField()
-    tag_id = models.ForeignKey(Tag, null=True,
-                               on_delete=models.CASCADE)
+    contributor = models.ForeignKey(User, null=False, related_name='contributor')
+    approver = models.ForeignKey(User, null=True, related_name='approver',on_delete=models.SET_NULL)
+    date_created = models.DateTimeField()
+    tag = models.ForeignKey(Tag, null=True,on_delete=models.SET_NULL)
     user_list = models.ManyToManyField(User)
 
     def __str__(self):
@@ -49,9 +48,9 @@ class ExerciseWebServer(models.Model):
 class ErrorMessage(models.Model):
     title = models.CharField(max_length=30, )
     content = models.CharField(max_length=255, )
-    reporter_id = models.ForeignKey(User,
+    reporter = models.ForeignKey(User,
                                     on_delete=models.CASCADE)
-    reported_exercise_id = models.ForeignKey(ExerciseWebServer,
+    exercise_report = models.ForeignKey(ExerciseWebServer,
                                              on_delete=models.CASCADE)
 
     def __str__(self):
